f8626f4702bf39d1b3a745b50224e01e49d1ca20
Vu <nguyenvu18889@gmail.com>
2019-10-21 16:21:09 +0700

    del array in c++ with new() and delete()

--- del_array_of_objects.cpp
+++ del_array_of_objects.cpp
@@ -24,10 +24,29 @@ public:
 int main()
 {
 //CREATE AN OBJECT DYNAMICALLY
-   Pen* pen = new Pen();
-   pen->write();//operations
+//    Pen* pen = new Pen();
+//    pen->write();//operations
+//    delete pen;//de-allocate the memory 
+    /* CONCULSION 1
+        - we must delete it explicitly to avoid memory leaks in C++ programs after we are done with its operations.
+    */   
+    
+
+
+//CREATE AN ARRAY OF OBJECTS
+   Pen* penList = new Pen[3];
+
+   penList[0].write();
+   penList[1].write();
+   penList[2].write();
    
-   delete pen;//de-allocate the memory
-   
+   delete penList;//de-allocate only penList[0] --> call Destructor once then crash
+//    delete [] penList;//de-allocate array of objects
+
+    /* CONCULSION 2
+        - You can only call delete[] on pointers returned by new[], and you must do this exactly once. 
+        - The same applies for delete and new.
+    */   
+
    return 0;
 }

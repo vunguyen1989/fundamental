886980b9b87104f2b6814ccb12bd15140812fa7a
Vu Nguyen <nguyenvu18889@gmail.com>
2020-09-29 18:32:22 +0700

    pointer as a parameter

---
+++ pointer_cheat_sheet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void PointerPara(int *&p)
+{
+    p = new int(3);
+}
+
+int main()
+{
+    //Pointer as a parameter
+    int *ptr = NULL;
+    printf("value of ptr = %p\n", ptr); //nil
+    PointerPara(ptr);
+    printf("value of ptr = %p\n", ptr); //nil
+}
